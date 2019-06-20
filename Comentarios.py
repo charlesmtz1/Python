ce9b20ac9ffd3a81b519cd15d0bfd6da32537f1c
Carlos Martinez <charlesmtz1@gmail.com>
2019-06-20 17:53:07 -0500

    Curso Python
    w3schools

---
+++ Comentarios.py
@@ -0,0 +1,14 @@
+#Esto es un comentario en Python
+
+#Esto es
+#un comentario
+#multilinea en Python
+
+"""
+Este comentario
+es multilinea
+y permitido en Python
+"""
+
+print("Seccion de comentarios.")
+print("Algunas reglas acerca de los comentarios estan dentro de este archivo.")
---
+++ Variables.py
@@ -0,0 +1,17 @@
+#Variables en Python
+
+entero = 10 #Variable tipo Int
+flotante = 3.25 #Variable tipo float
+cadena = "Python" #Variable tipo String
+
+print(entero)
+print(flotante)
+print(cadena)
+
+x, y, z = 1, 2, 3
+
+print(x + y + z)
+
+edad = '26'
+
+print("La edad de Rei es " + edad) #Solo se pueden concatenar o sumar variables del mismo tipo
